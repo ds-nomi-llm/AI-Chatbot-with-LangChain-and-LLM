ea8b2f7eb98e3887ac7239c13346ae3fbc37d947
ds-nomi-llm <nomanahmed3694@gmail.com>
2025-08-07 00:27:28 +0500

    Delete backend directory

--- backend/langchain_app/embeddings.py
+++
@@ -1,8 +0,0 @@
-from langchain_openai import OpenAIEmbeddings
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-def get_embedding_model():
-    return OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
